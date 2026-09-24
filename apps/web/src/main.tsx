aba0b462c34a64b7e2280dcebd1174d94670d76b
Rurpaj Singh <ruprajsingh1@gmail.com>
2026-09-24 11:15:20 +0000

    feat: add Inter and JetBrains Mono fonts for improved typography

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -17,6 +17,8 @@
 
 import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
+import '@fontsource-variable/inter'
+import '@fontsource-variable/jetbrains-mono'
 import './index.css'
 import App from './App.tsx'
 import { queryClient } from './lib/queryClient.ts'
